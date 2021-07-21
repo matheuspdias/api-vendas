6b152cbacf061bb8dbfa950fa19fc8dcf0ba2e1a
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-07-20 22:13:40 -0300

    feat: create product service, entity and repository

---
+++ src/app/services/products/CreateProductService.ts
@@ -0,0 +1,33 @@
+import Product from '@models/Product';
+import AppError from '@shared/errors/AppError';
+import { ProductRepository } from 'src/app/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    const product = productRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
