53d99bc87c5b1e7b58299e76791b64cdee29c491
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-07-20 23:09:45 -0300

    chore: product services crud complete

---
+++ src/app/services/products/ShowProductService.ts
@@ -0,0 +1,24 @@
+import Product from '@models/Product';
+import AppError from '@shared/errors/AppError';
+import { ProductRepository } from 'src/app/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+
+interface IRequest {
+  id: number;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+
+    const product = await productRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;
